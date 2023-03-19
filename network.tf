202fecc71525de0b116b27692a9cc5d1da470f5a
root <root@ip-172-31-59-253.ec2.internal>
2023-03-19 01:34:33 +0000

    ADD: creating Sub Prin e PUB

--- network.tf
+++ network.tf
@@ -7,6 +7,28 @@ resource "aws_vpc" "vorx-vpc-prod" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+ tags = {
+   Name = "Public-Subnet-1a"
+
+  }
+}
+
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id     = aws_vpc.vorx-vpc-prod.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-1a"
+  }
+}
+
 
 
 
@@ -19,4 +41,6 @@ output "vpc_vorx_prod_arn" {
 value = aws_vpc.vorx-vpc-prod.arn
 }
 
-
+output "vorx_prod_subnet" {
+value = "aws_subnet.*.id"
+}
